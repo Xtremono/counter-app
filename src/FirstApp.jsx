5430ea1d7849c4ce13cd362f47bcf547a6e0556e
jose tomas ruiz <j_tomas_93@hotmail.com>
2024-11-11 21:23:17 -0300

    Counter react app

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,10 +1,30 @@
-export const FirstApp = () => {
+import PropTypes from "prop-types";
+
+
+export const FirstApp = ({title, subTitle, namee }) => {
+
+
   return (
     <>
-    <h1>Soy Tomas</h1>
-    <p>Estoy tratando de conseguir mi primer trabajo en el mundo tech</p>
+    <h1>{title}</h1>
+    <p>{subTitle}</p>
+    <p>{namee}</p>
     </>
   );
 };
 
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,                        //
+  subTitle: PropTypes.number.isRequired
+};
+
+FirstApp.defaultProps = {
+  title: "No hay titulo",
+  subTitle: 31,
+  namee: "Jose Tomas"
+};
+
 // <></> A esto se le llama fragmento, es un agrupador de elementos html. Esto tambien lo conocemos como "Nodo Padre"
+// {Dentro de esta llaves no podemos colocar un objeto} <h1>{{nombre:tomas, apellido: ruiz}}</h1>. Esto no es valido.
+//Podriamos llamar al objeto si queremos con puntuacion. Digamos que nuestro objeto esta en const me = {nombre:tomas, apellido: ruiz}. Lo llamariamos <h1>{me.nombre}</h1>
+// El tittle es un props, una propiedad. Defini esa propiedad en el archivo main.jsx en la parte de mi componente <FirstApp title="Hola soy Tomas!"/>
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 //import { HelloWorldApp } from './HelloWorldApp';
 import { FirstApp } from './FirstApp'  // Importación nombrada
+import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FirstApp />
+    <FirstApp  subTitle={30}/>
   </React.StrictMode>
 );
