04ba911d3001df4448ff44145589154d5b14ca49
jose tomas ruiz <j_tomas_93@hotmail.com>
2024-11-12 14:14:19 -0300

    Counter react app

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 //import { HelloWorldApp } from './HelloWorldApp';
-import { FirstApp } from './FirstApp'  // Importación nombrada
+//import { FirstApp } from './FirstApp'  // Importación nombrada
 import "./styles.css";
+import { CounterApp } from './CounterApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FirstApp  subTitle={30}/>
+    <CounterApp  value={0}/>
   </React.StrictMode>
 );
