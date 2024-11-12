213ef32b96200e890945ec25c332e4afb19ddbdc
jose tomas ruiz <j_tomas_93@hotmail.com>
2024-11-12 19:21:03 -0300

    Last commit,  with useState Hook

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
+
 
 export const CounterApp = ({value}) => {
 
+  const [ counter, setCounter ] = useState( value );
+  const handleAdd = () => setCounter( counter + 1);
+  const handleSubstract = () => setCounter(counter -1);
+  const handleReset = () => setCounter(value);
+
   return (
     <>
-    <h1>CounterApp</h1>
-    <h2>{value}</h2>
+      <h1>CounterApp</h1>
+      <h2>{counter}</h2>
+
+      <button onClick={handleAdd}>+1</button>
+      <button onClick={handleSubstract}>-1</button>
+      <button onClick={handleReset}>RESET</button>
     </>
   )
 };
@@ -14,6 +25,9 @@ CounterApp.propTypes = {
   value: PropTypes.number
 };
 
-CounterApp.defaultProps = {
-  value: 0
-};
+
+// usaamos onClick, para notar que cada vez que se presiona el boton se genera una acccion
+// const [ counter, setCounter ] = useState( 0 );                 1. INICIALIZAMOS EL VALOR DE COUNTER EN 0 EN "USESTATE()""
+  //const handleAdd = (event) => {                                2. DENTRO DE NUESTRO METODO HANDLEADD LLAMAMOS A LA FUNCION SETCOUNTER
+  //  setCounter( counter + 1);                                   3. DENTRO DE SETCOUNTER LE AGREGAMOS EL VALOR DE COUNTER +1;
+ // }
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,6 @@ import { CounterApp } from './CounterApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CounterApp  value={0}/>
+    <CounterApp value={0}/>
   </React.StrictMode>
 );
