097394ef9c10e9a04357140f9970cc1b3b951e1a
jose tomas ruiz <j_tomas_93@hotmail.com>
2024-11-17 12:56:26 -0300

    Snapshot

--- src/test/FirstApp.test.jsx
+++ src/test/FirstApp.test.jsx
@@ -9,10 +9,23 @@ describe('FirstApp', () => {
 
   test('Debe hacer match con el snapshot', () => {
       const title = "Hola, soy Tomas"
-      render(<FirstApp title={title}/>)
+      const {container} = render(<FirstApp title={title}/>)
 
-
+      expect(container).toMatchSnapshot();
 
 
   })
+
+  test('debe de mostrar el titulo en un h1', () => {
+    const title = "Hola, soy Tomas"
+    const {container, getByText} = render(<FirstApp title={title}/>)
+    expect(getByText(title)).toBeTruthy();
+
+    const h1 = container.querySelector("h1");
+    expect(h1.innerHTML).toBe(title);
+
+
+
+
+   })
 })
